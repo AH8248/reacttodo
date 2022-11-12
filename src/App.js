f4dbeb210dfa8f235a71af72d5594a3e7e789e79
AH8248 <alhamza460@gmail.com>
2022-11-12 20:49:28 +0500

    ALAE

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 // import Tabs from "./component/TabMenu/Tabs";
 import GalleryReact from "./component/TabMenu/GalleryReact";
+import Testtodo from './component/todoProject/testtodo';
 
 const App = () => {
   return (
     <>
-     <GalleryReact />
+     <Testtodo/>
     </>
   )
 }
